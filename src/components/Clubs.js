aecbf7da260d6a2f29bf1b461bd552df8c827417
SREEKAR2499 <vsssreekar2499@gmail.com>
2023-10-14 22:45:12 +0530

    first commit

---
+++ src/components/Clubs.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Clubs = () => {
+  return (
+    <section>
+            <h2>Clubs & Organizations</h2>
+            <ul>
+                <li>TAM    (Technical) - Core Member - 2021</li>
+                <li>Sahiti (Cultural) - Cultural Head - 2021</li>
+                <li> Worked with the Publicity and Marketing team for RIVIERA 2023.</li>
+            </ul>
+    </section>
+  );
+}
+
+export default Clubs;
+
